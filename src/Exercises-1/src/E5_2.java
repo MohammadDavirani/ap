3b94501a4e27589cc3817b1cc1f21f8304f4f92f
mohammad <dabirimohammad147@gmail.com>
2025-02-27 22:43:28 +0330

    E5_2

---
+++ src/Exercises-1/src/E5_2.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class E5_2
+{
+    public static void main(String[] args)
+    {
+        System.out.print("Enter a number:");
+        Scanner number = new Scanner(System.in);
+
+        double num = number.nextDouble();
+        if(num==0)
+        {
+            System.out.println("The number is zero.");
+        } else if(num>0)
+        {
+          System.out.println("The number is positive.");
+        }
+        else
+        {
+            System.out.println("The number is negative.");
+
+        }
+
+
+        double absNum = Math.abs(num);
+        if(absNum<1)
+        {
+            System.out.println("The number is less than 1 so  it's-> Small ");
+        }else if(absNum > 1000000)
+        {
+            System.out.println("The number is greater than 1 million, so it's -> Large ");
+
+        }
+
+    }
+}
