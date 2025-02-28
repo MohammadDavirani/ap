b567e2b19cc51c6ae506aa62451a3a740a800a7f
mohammad <dabirimohammad147@gmail.com>
2025-03-01 01:09:05 +0330

    E5_2

---
+++ src/E5_18.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+
+public class E5_18 {
+    public static void main(String[] args) {
+        System.out.println("=====Sorting Program=====");
+        System.out.println("Please enter three word:");
+
+        String[] words = new String[3];
+        Scanner input = new Scanner(System.in);
+        
+        for (int i = 0; i < 3; i++) {
+            System.out.format("%d) word: ", i + 1);
+            words[i] = input.nextLine();
+        }
+
+        for(int i=0;i<3;i++) {
+                for(int j=0;j<2;j++) {
+                    if(words[i].compareTo(words[j])<0) {
+                        String temp = words[i];
+                        words[i] = words[j];
+                        words[j] = temp;
+                    }
+                }
+        }
+
+        System.out.println("Sorted words!");
+        for(int i=0;i<3;i++) {
+            System.out.format("%d. %s\n",i+1,words[i]);
+        }
+    }
+}
