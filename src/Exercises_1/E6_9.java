b4cdd9278b8223c4063253f44e4c8a54fcea5676
mohammad <dabirimohammad147@gmail.com>
2025-03-03 23:53:44 +0330

    E6_9

---
+++ src/Exercises_1/E6_9.java
@@ -0,0 +1,19 @@
+package Exercises_1;
+import java.util.Scanner;
+
+public class E6_9 {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("please enter a word: ");
+        String word = input.nextLine();
+
+        int length = word.length();
+
+        for(int i=length-1;i>=0;i--) {
+            char ch = word.charAt(i);
+            System.out.format("%c ",ch);
+        }
+    }
+}
