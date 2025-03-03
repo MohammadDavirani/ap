1c9194f946755287de2602f17545cb9f0ba2f3a6
mohammad <dabirimohammad147@gmail.com>
2025-03-04 00:39:23 +0330

    E6_13

---
+++ src/Exercises_1/E6_13.java
@@ -0,0 +1,22 @@
+package Exercises_1;
+import java.util.Scanner;
+
+public class E6_13 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("please enter a number: ");
+        int number = input.nextInt();
+
+        int reminder,external;
+
+        while(number>0)
+        {
+            reminder = number % 2;
+            System.out.format("%d \n",reminder);
+
+            external = number / 2;
+            number =  external;
+        }
+    }
+}
