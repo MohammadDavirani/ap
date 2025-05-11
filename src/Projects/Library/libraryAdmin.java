ac1fc27f6bd0f5e1264991b167a3064eaac955ff
mohammad <dabirimohammad147@gmail.com>
2025-05-11 08:07:12 +0330

    library02

---
+++ src/Projects/Library/libraryAdmin.java
@@ -0,0 +1,27 @@
+package Projects.Library;
+
+public class libraryAdmin {
+    private String firstName;
+    private String lastName;
+    private String educationLevel;
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    public void setEducationLevel(String educationLevel) {
+        this.educationLevel = educationLevel;
+    }
+
+    public static void getOverdueLoans(){
+
+    }
+    public static void getLoanStatsPerManager(){
+
+    }
+    public static void getTop10LoanedBooks(){
+
+    }
+}
