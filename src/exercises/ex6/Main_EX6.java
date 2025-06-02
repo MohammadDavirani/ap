18e5faa0c7bda3f8900a5e873c951de7f267a1e3
mohammad <dabirimohammad147@gmail.com>
2025-06-03 00:13:56 +0330

    Main_EX6

---
+++ src/exercises/ex6/Main_EX6.java
@@ -0,0 +1,61 @@
+package exercises.ex6;
+
+import exercises.ex6.scraper.Conf;
+import exercises.ex6.scraper.utils.DirectoryTools;
+import exercises.ex6.scraper.utils.FileTools;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Main_EX6 {
+
+    private static List<String> fileList = DirectoryTools.getFilesAbsolutePathInDirectory(Conf.SAVE_DIRECTORY);
+
+    public static List<String> getAllUrls() {
+        List<String> urls = fileList.stream()
+                .map(fileAddress -> FileTools.getTextFileLines(fileAddress))
+                .filter(s -> s != null)
+                .flatMap(s -> s.stream())
+                .map(s -> getFirstImageSrc(s))
+                .filter(s -> s != null)
+                .filter(s -> s.length() > 1)
+                .filter(s -> !s.contains(".js"))
+                .filter(s -> !s.contains(".mp4"))
+                .distinct()
+                .collect(Collectors.toList());
+        return urls;
+    }
+    public static String getFirstImageSrc(String htmlLine) {
+        String url = null;
+        int startIndex = htmlLine.indexOf("src=\"");
+        if (startIndex >= 0) {
+            try {
+                int srcLength = "src=\"".length();
+                int endIndex = htmlLine.indexOf("\"", startIndex + srcLength + 1);
+                url = htmlLine.substring(startIndex + srcLength + 1, endIndex);
+            } catch (Exception e) {
+            }
+        }
+        return url;
+    }
+
+    public static void saveImageUrls(){
+        List<String> urls = getAllUrls();
+        try{
+            FileWriter writer = new FileWriter("ImageUrls.txt");
+            for (String allUrl : urls) {
+                writer.write(allUrl+"\n");
+            }
+            writer.close();
+            System.out.println("Image Urls wrote to file Successful");
+        }catch(IOException e){
+            System.out.println("Error:"+e.getMessage());
+        }
+    }
+
+    public static void main(String[] args) {
+        saveImageUrls();
+    }
+}
